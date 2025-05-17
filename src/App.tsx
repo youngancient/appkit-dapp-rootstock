4620008eac8ec9db7dcd959b2981b0ddaa9becb2
youngancient <tochyokoye@gmail.com>
2025-05-17 14:52:45 +0100

    UI in progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import "./connection.ts";
-import { useAppKitAccount } from "@reown/appkit/react";
+import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
 import { formatAddress } from "./utils.ts";
 import { useState } from "react";
 import { ethers } from "ethers";
@@ -17,7 +17,10 @@ function App() {
   const { isConnected, address } = useAppKitAccount();
   const [tokenDetail] = useState<ITokenDetail | null>(null);
   const [tokenBalance] = useState<number | null>(null);
-  
+
+  // controls popup of wallet connect monal
+  const { open } = useAppKit();
+
   // mock loading state for read contract calls
   const [isLoadingBalance] = useState(false);
   const [isLoadingDetails] = useState(false);
@@ -38,17 +41,15 @@ function App() {
       </div>
       <div className="flex mt">
         {isConnected && <p>Token Balance: {12000}</p>}
-        {isConnected && (
-          <button onClick={() => console.log("hello world")}>Send Token</button>
-        )}
       </div>
       <div className="flex mt">
         <div>
           {isConnected ? (
-            isLoadingBalance ? (
-              <p>Loading...</p>
-            ) : (
-              <div>
+            <div>
+              {/* Token Balance Section */}
+              {isLoadingBalance ? (
+                <p>Loading balance...</p>
+              ) : (
                 <p>
                   Token Balance:{" "}
                   {tokenBalance != null
@@ -57,12 +58,22 @@ function App() {
                       ).toLocaleString()
                     : 0}
                 </p>
-                <p>Token Name: {tokenDetail?.name}</p>
-                <p>Token symbol: {tokenDetail?.symbol}</p>
-                <p>Token currentSupply: {tokenDetail?.currentSupply}</p>
-                <p>MAX_SUPPLY: {tokenDetail?.maxSupply}</p>
-              </div>
-            )
+              )}
+
+              {/* Token Detail Section */}
+              {isLoadingDetails ? (
+                <p>Loading token details...</p>
+              ) : (
+                tokenDetail && (
+                  <div>
+                    <p>Token Name: {tokenDetail.name}</p>
+                    <p>Token Symbol: {tokenDetail.symbol}</p>
+                    <p>Token Current Supply: {tokenDetail.currentSupply}</p>
+                    <p>MAX_SUPPLY: {tokenDetail.maxSupply}</p>
+                  </div>
+                )
+              )}
+            </div>
           ) : (
             <p>Please connect your wallet to see token details.</p>
           )}
